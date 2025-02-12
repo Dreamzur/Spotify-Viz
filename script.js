23d0ae89fb23f491815b62f845edd0867456daf5
Dahiver Guerra <roddavid1@gmail.com>
2025-02-12 10:13:55 -0500

    Added API use for the search function

--- script.js
+++ script.js
@@ -1,4 +1,103 @@
 import { songList } from './spotify_top_hits_clean_json.js';
+import API_KEY from './config.js';
+
+function showForm() {
+    const resultCard = document.getElementById('result-card')
+    resultCard.innerHTML = ''
+    const artistSearch = document.createElement('div')
+    artistSearch.innerHTML =
+    `
+    <form id="artist-search-form">
+        <label for="artist-input">Search for an artist</label>
+        <input type="text" name="artist" id="artist-input" placeholder="Taylor Swift">
+        <button type="submit" id="submit-button">Search</button>
+    </form>
+    `
+    resultCard.prepend(artistSearch)
+    
+    const form = document.getElementById('artist-search-form')
+    form.addEventListener('submit', handleSubmit)
+}
+
+const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const inputForm = document.getElementById('artist-input')
+    const artistRequested = inputForm.value
+
+    const artists = await searchArtists(artistRequested)
+    displayArtistCards(artists)
+
+}
+
+async function searchArtists(artist) { // search for an artist using spotify's API
+    const requestURL = `https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=5&offset=0`
+    const details = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${API_KEY}`
+        }
+    }
+
+    const result = await fetch(requestURL, details)
+    const json = await result.json()
+    console.log(json)
+
+    const retrievedArtists = json.artists.items
+    const cleanedArtistData = retrievedArtists.map((artist) => (
+        {
+            id: artist.id,
+            name: artist.name,
+            link: artist.external_urls.spotify,
+            followers: artist.followers.total,
+            popularity: artist.popularity,
+            genre: artist.genres,
+            image: (artist.images.length > 0 ? artist.images[0].url : '')
+        }
+    ))
+
+    return cleanedArtistData
+}
+
+function displayArtistCards(artistDetails) {
+    const resultCard = document.getElementById('result-card')
+    resultCard.innerHTML = ''
+    showForm()
+
+    console.log("Artist details : ", artistDetails)
+
+    const cardsContainer = document.createElement('div')
+    artistDetails.forEach((artist) => {
+        const card = document.createElement('div')
+        card.innerHTML = 
+            `
+            <div class="card-container">
+            <div class="detail-container">
+                <div class="img-container">
+                    <img src=${artist.image ? artist.image : 'https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green.png'} alt=${artist.name}/>
+                </div>
+                <div class="info-container">
+                    <h4>
+                        ${artist.name}
+                    </h4>
+                    <div>
+                        <span>${artist.genre} ◆ </span>
+                        <span>${artist.followers} Followers ◆ </span>
+                        <span>Popularity ${artist.popularity}</span>
+                    </div>
+                </div>
+            </div>
+            <h6 class="visit-link"><a href=${artist.link}>Visit Artist</a></h6>
+            </div>
+            `
+            
+            card.addEventListener('click', () => {handleArtistClick(artist.id)})
+            cardsContainer.appendChild(card)
+        })
+        
+        resultCard.append(cardsContainer)
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.result-button');
@@ -14,6 +113,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 createYearChart();
             } else if (buttonText === 'Loudest Song') {
                 createLoudestSongChart();
+            } else if (buttonText == 'Search') {
+                showForm()
             }
         });
     });
